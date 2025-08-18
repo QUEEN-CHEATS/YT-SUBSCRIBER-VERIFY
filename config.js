6b5a39c800e86bc8d3090092ba9c7c54ee353c73
QUEEN KHUSHI <pkumaran285@gmail.com>
2025-08-18 18:36:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "your-bot-token-here", // Add your bot's token here (REQUIRED)
-    channel_name: "@devuuuu_xd", // Specify your YouTube channel's name here (REQUIRED)
-    role_id: "", // ID of the role you want to assign (OPTIONAL)
+    token: "MTM5OTI3NzA2MzQ1Nzg2NTgzOQ.GaTwc8.3gvNHIp_pwneHeZnbhQCbfjVPWl3C-hN_jMD4s", // Add your bot's token here (REQUIRED)
+    channel_name: "QUEEN CHEATS", // Specify your YouTube channel's name here (REQUIRED)
+    role_id: "1406987298704461844", // ID of the role you want to assign (OPTIONAL)
     keywords: "subscribe, follow, like", // Specify the keywords separated by commas (OPTIONAL)
     save_data: "true" // Save data in subscriber.json (OPTIONAL)
 };
